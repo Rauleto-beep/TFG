b34b343648b52555ad7251f6735a94d395b8ed23
Rauleto <rauletillorastrollito@gmail.com>
2026-02-11 16:41:31 +0000

    Creado el PDO y controlador para mensajes

--- src/Controller/MensajeController.php
+++ src/Controller/MensajeController.php
@@ -2,18 +2,51 @@
 
 namespace App\Controller;
 
+use App\Repository\MensajeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MensajeController extends AbstractController
 {
-    #[Route('/mensaje', name: 'app_mensaje')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MensajeController.php',
-        ]);
+    #[Route('api/mensaje/ver', name: 'app_mensaje_ver')]
+    public function verMensaje(MensajeRepository $repo):JsonResponse{
+        try{
+            $mensajes = $repo->verMensaje();
+            return $this->json($mensajes);
+        }catch(\Exception $e){
+            return $this->json([ 'message' => 'Error al mostrar los mensajes']);
+        }
+    }
+
+    #[Route('api/mensaje/crear', name: 'app_mensaje_crear')]
+    public function crearMensaje(Request $request,MensajeRepository $repo):JsonResponse{
+        try{
+            $datos = json_decode($request->getContent(),true);
+            $repo->crearMensaje($datos);
+            if (!isset($datos['contenido'])){
+                return $this->json([ 'message' => 'No puede estar vacio el mensaje']);
+            }else{
+                return $this->json([ 'message' => 'Mensaje creado']);
+            }
+        }catch(\Exception $e){
+            return $this->json([ 'message' => 'Error al crear el mensaje']);
+        } 
+    }
+
+    #[Route('api/mensaje/eliminar', name: 'app_mensaje_eliminar')]
+    public function eliminarMensaje(Request $request,MensajeRepository $repo):JsonResponse{
+        try{
+            $datos = json_decode($request->getContent(),true);
+            $repo->eliminarMensaje($datos['id']);
+            if (!isset($datos['id'])){
+                return $this->json([ 'message' => 'Selecciona el mensaje a eliminar']);
+            }else{
+                return $this->json([ 'message' => 'Mensaje eliminado']);
+            }
+        }catch(\Exception $e){
+            return $this->json([ 'message' => 'Error al eliminar el mensaje']);
+        } 
     }
 }
--- src/Repository/MensajeRepository.php
+++ src/Repository/MensajeRepository.php
@@ -15,29 +15,42 @@ class MensajeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Mensaje::class);
     }
+    private function ejecutarYFormatear(string $sql): array{
+    $resultados = $this->getEntityManager()->getConnection()->executeQuery($sql)->fetchAllAssociative();
 
-    //    /**
-    //     * @return Mensaje[] Returns an array of Mensaje objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    foreach ($resultados as &$tarea) {
+        
+        if ($tarea['fecha_envio']) {
+            $tarea['fecha_envio'] = (new \DateTime($tarea['fecha_envio']))->format('d-m-Y H:i:s');
+        }
+    }
 
-    //    public function findOneBySomeField($value): ?Mensaje
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    return $resultados;
+    }
+
+    public function verMensaje(){
+        $sql = "SELECT id,contenido,fecha_envio,grupo_id,autor_id FROM mensaje";
+        return $this->ejecutarYFormatear($sql);
+    }
+
+    public function crearMensaje(array $datos){
+        $sql="INSERT INTO mensaje (contenido,fecha_envio,grupo_id,autor_id) 
+              VALUES (:contenido,:fEnv,:grupo_id,:autor_id)";
+        $params=[
+            "contenido" => $datos['contenido'],
+            "fEnv" => $datos['fecha_envio'],
+            "grupo_id" => $datos['grupo_id'],
+            "autor_id" => $datos['autor_id']
+        ];
+
+        return $this->getEntityManager()->getConnection()->executeQuery($sql, $params);
+    }
+
+    public function eliminarMensaje(int $id){
+        $sql="DELETE FROM mensaje WHERE id = :id";
+        $params=[
+            "id" => $id
+        ];
+        return $this->getEntityManager()->getConnection()->executeQuery($sql, $params);
+    }
 }
